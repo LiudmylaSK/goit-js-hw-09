e9649fd7c4abdd9f97d5fdc32f161d7a3016ac05
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-09-02 18:13:29 +0300

    updates Task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.body;
+
+let intervalId = null;
+
+const startColorChange = () => {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }
+};
+
+const stopColorChange = () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  }
+};
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
