8112c53259228c704042e50db5af37da49565ca6
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-09-02 21:56:05 +0300

    updates Task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function handlePromiseSuccess({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function handlePromiseError({ position, delay }) {
+  Notiflix.Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  let delay = parseInt(formData.get('delay'));
+  const step = parseInt(formData.get('step'));
+  const amount = parseInt(formData.get('amount'));
+
+  const promises = [];
+  for (let i = 0; i < amount; i++) {
+    promises.push(createPromise(i + 1, delay));
+    delay += step;
+  }
+
+  promises.forEach(promise => {
+    promise.then(handlePromiseSuccess).catch(handlePromiseError);
+  });
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleFormSubmit);
